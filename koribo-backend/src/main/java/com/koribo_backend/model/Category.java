e9c46bdf02e1ba177249f5f4ec73af947a685d03
Jennifer Okeke <aestheticjenny04@gmail.com>
2025-03-26 10:00:41 +0000

    implement category model

---
+++ koribo-backend/src/main/java/com/koribo_backend/model/Category.java
@@ -0,0 +1,35 @@
+package com.koribo_backend.model;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor
+@Setter
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Flashcard> flashcards;
+}
