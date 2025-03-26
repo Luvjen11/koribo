d38c077852c3077601f1377a5cf4515495986a5b
Jennifer Okeke <aestheticjenny04@gmail.com>
2025-03-26 09:49:06 +0000

    implement flashcard repository

---
+++ koribo-backend/src/main/java/com/koribo_backend/repository/FlashcardRepository.java
@@ -0,0 +1,7 @@
+package com.koribo_backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FlashcardRepository extends JpaRepository<Flashcard, Long>{
+    
+}
